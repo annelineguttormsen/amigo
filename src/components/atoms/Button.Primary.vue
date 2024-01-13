89dc2899c8e3ac00e642607baa8c63b4d3dd6129
Anneline Guttormsen <anneline.guttormsen@hotmail.no>
2024-01-13 23:37:57 +0100

    chore: add basic primary button

---
+++ src/components/atoms/Button.Primary.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+const props = defineProps({
+  type: {
+    type: String,
+    required: false,
+    default: "button"
+  }
+})
+</script>
+
+<template>
+  <button
+    class="
+      bg-white inline-flex cursor-pointer px-1 py-0.5 bg-white rounded-md font-semibold border-2 border-solid border-black text-base leading-tight outline-none
+    "
+    :type="props.type"
+  >
+    <slot />
+  </button>
+</template>
