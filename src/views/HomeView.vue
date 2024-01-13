5722ed4c64978f018d62cbe047e59a5e633efed4
Anneline Guttormsen <anneline.guttormsen@hotmail.no>
2024-01-13 16:12:21 +0100

    chore: update home view to include basic create project and see stored projects

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,48 @@
+<script lang="ts" setup>
+  import { ref, computed } from 'vue'
+  import { makeNewProject } from '@/util/projects.util'
+  import { getItem } from '@/util/localstorage.util'
+  import Counter from '@/components/Counter.vue'
+  import { StorageCrochetId, type CrochetProject } from '@/interfaces/globalInterfaces'
+
+  const counterValue = ref(0)
+
+  function newProject() {
+    makeNewProject("TEST", 10, 10)
+  }
+
+  const crochetProjects= computed<CrochetProject[] | undefined>(() =>{
+    const crochetProjectIds = getItem(StorageCrochetId.ListOfCrochetProjects)
+    if (crochetProjectIds == undefined) {
+      return undefined
+    }
+    const crochetProjects : CrochetProject[] | [] = []
+    crochetProjectIds.forEach(id => {
+      crochetProjects.push(getItem(`${StorageCrochetId.CrochetProject}#${id}`))
+    });
+    return crochetProjects
+  })
+</script>
+
 <template>
-  <h1>This is a homepage</h1>
+  <Counter v-model="counterValue"/>
+  <button
+    @click="newProject()"
+  >
+    Lag prosjekt
+  </button>
+  <p
+    v-if="crochetProjects == undefined"
+  >
+    Ingen lagrede prosjekter
+  </p>
+  <div
+    v-else
+  >
+    <button
+      v-for="(crochetProject, index) in crochetProjects"
+    >
+      Åpne opp prosjekt {{ crochetProject?.name }}
+    </button>
+  </div>
 </template>
