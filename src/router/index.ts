873b728f68f403cca526dc5576b7584dc0217a59
Anneline Guttormsen <anneline.guttormsen@hotmail.no>
2024-01-13 16:01:46 +0100

    build: add project view file to router

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,12 +10,12 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/project',
+      name: 'project',
+      props: (route) => ({
+        id: route.query.id
+      }),
+      component: () => import('../views/ProjectView.vue')
     }
   ]
 })
---
+++ src/views/ProjectView.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import { defineProps } from 'vue'
+import { getItem } from '../util/localstorage.util'
+import { CrochetProject, StorageCrochetId } from '@/interfaces/globalInterfaces'
+
+const props = defineProps({
+  id: {
+    type: String,
+    required: true
+  }
+})
+
+const currentProject:CrochetProject | undefined = getItem(`${StorageCrochetId.CrochetProject}#${props.id}`)
+</script>
+
+<template>
+  <p v-if="currentProject == undefined">
+    Ingen prosjekter under denne ID'en: {{ props?.id }}
+  </p>
+  <p v-else>
+    {{ currentProject.name }}
+  </p>
+
+</template>
