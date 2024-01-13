4f539f65837764b73824d68a7c3797246a66e5ef
Anneline Guttormsen <anneline.guttormsen@hotmail.no>
2024-01-13 23:17:10 +0100

    chore: add create new project function inside a dialog in home view

--- src/views/Home/HomeView.vue
+++ src/views/Home/HomeView.vue
@@ -1,36 +1,38 @@
 <script lang="ts" setup>
   import { ref } from 'vue'
-  import { RouterLink } from 'vue-router'
+  import { useRouter, RouterLink } from 'vue-router'
   import { makeNewProject } from '@/util/projects.util'
   import { getItem } from '@/util/localstorage.util'
   import { StorageCrochetId } from '@/interfaces/globalInterfaces'
+  import { useField } from 'vee-validate'
+  import { stringRequiredSchema, numberRequiredSchema } from '@/validation/schema'
   import Dialog from '@/components/organisms/Dialog.vue'
   import Input from '@/components/atoms/input/Input.vue'
 
-  function newProject() {
-    makeNewProject("TEST", 10, 10)
-  }
-
+  const router = useRouter()
+  
   const crochetProjectIds = getItem(StorageCrochetId.ListOfCrochetProjects)
-
+  
   const showCreateProjectDialog = ref(false)
+  
+  const { value: projectNameValue, errorMessage: projectNameError, validate: projectNameValidate } = useField('projectname', stringRequiredSchema('Name'))
+  const { value: projectRowsValue, errorMessage: projectRowsError, validate: projectRowsValidate } = useField('projectname', numberRequiredSchema(0, 1000, 'Rows'))
+  const { value: projectStitchesValue, errorMessage: projectStitchesError, validate: projectStitchesValidate } = useField('projectname', numberRequiredSchema(0, 1000, 'Stitches'))
+
+  async function validateAndCreateProject() : Promise<void> {
+    // TODO: redo this validation mess, convert into useForm
+    await projectNameValidate()
+    await projectRowsValidate()
+    await projectStitchesValidate()
+
+    if ([projectNameError, projectRowsError, projectStitchesError].every(err => err.value == undefined)) {
+      const id = makeNewProject(projectNameValue.value, projectRowsValue.value, projectStitchesValue.value)
+      router.push(`/project?id=${id}`)
+    }
+  }
 </script>
 
 <template>
-  <Dialog
-    v-model="showCreateProjectDialog"
-  >
-    <template #header>
-      Create new project
-    </template>
-    <template #content>
-      Hey baby girl
-      <Input 
-        v-model="projectName"
-        label="Project name"
-      />
-    </template>
-  </Dialog>
   <button
     @click="showCreateProjectDialog = true"
     class="p-0.25"
@@ -53,4 +55,36 @@
       Åpne opp prosjekt {{ getItem(`${StorageCrochetId.CrochetProject}#${id}`)?.name }}
     </RouterLink>
   </div>
+  <Dialog
+    v-model="showCreateProjectDialog"
+  >
+    <template #header>
+      Create new project
+    </template>
+    <template #content>
+      <Input 
+        v-model="projectNameValue"
+        label="Project name"
+        :error="projectNameError"
+        :required="true"
+      />
+      <Input 
+        v-model="projectRowsValue"
+        label="Amount of rows"
+        :error="projectRowsError"
+        :required="true"
+      />
+      <Input 
+        v-model="projectStitchesValue"
+        label="Stitches per row"
+        :error="projectStitchesError"
+        :required="true"
+      />
+      <button
+        @click="validateAndCreateProject()"
+      >
+        Create project
+      </button>
+    </template>
+  </Dialog>
 </template>
