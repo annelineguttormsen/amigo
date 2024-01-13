4285267aceb1678a3befa05feeef5787213dba9a
Anneline Guttormsen <anneline.guttormsen@hotmail.no>
2024-01-13 19:32:40 +0100

    build: move home view file into its own home folder

--- src/views/Home/HomeView.vue
+++ src/views/Home/HomeView.vue
@@ -1,19 +1,39 @@
 <script lang="ts" setup>
+  import { ref } from 'vue'
   import { RouterLink } from 'vue-router'
   import { makeNewProject } from '@/util/projects.util'
   import { getItem } from '@/util/localstorage.util'
   import { StorageCrochetId } from '@/interfaces/globalInterfaces'
+  import Dialog from '@/components/organisms/Dialog.vue'
+  import Input from '@/components/atoms/input/Input.vue'
 
   function newProject() {
     makeNewProject("TEST", 10, 10)
   }
 
   const crochetProjectIds = getItem(StorageCrochetId.ListOfCrochetProjects)
+
+  const showCreateProjectDialog = ref(false)
 </script>
 
 <template>
+  <Dialog
+    v-model="showCreateProjectDialog"
+  >
+    <template #header>
+      Create new project
+    </template>
+    <template #content>
+      Hey baby girl
+      <Input 
+        v-model="projectName"
+        label="Project name"
+      />
+    </template>
+  </Dialog>
   <button
-    @click="newProject()"
+    @click="showCreateProjectDialog = true"
+    class="p-0.25"
   >
     Lag prosjekt
   </button>
