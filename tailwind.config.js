30071fdaa16c22624166806f1161c37f5334e16f
Anneline Guttormsen <anneline.guttormsen@hotmail.no>
2024-01-13 18:28:29 +0100

    config: add generate sizing values in tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,12 @@
+function generateSizing(maxNumber, characterType) {
+  // generate sizings, that include decimal numbers (x.25, x.5, x.75)
+  const sizings = {}
+  for (let i = 0; i <= maxNumber; i += 0.25) {
+    sizings[i.toString()] = `${i}${characterType}`
+  }
+  return sizings
+}
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -5,6 +14,7 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    spacing: generateSizing(6, 'rem'),
     extend: {},
   },
   plugins: [],
