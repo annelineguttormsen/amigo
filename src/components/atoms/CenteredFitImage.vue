cd0e10f2df9aa340be080ea9bd5b9d8c9e5b131a
Anneline Guttormsen <anneline.guttormsen@hotmail.no>
2024-06-27 16:50:17 +0200

    build: add basic responsive centered fit image component

---
+++ src/components/atoms/CenteredFitImage.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+const props = defineProps({
+  src: {
+    type: String,
+    required: true
+  },
+  srcset: {
+    type: String,
+    required: false
+  },
+  sizes: {
+    type: String,
+    required: false
+  },
+  alt: {
+    type: String,
+    required: true
+  },
+  // class prop is used to determine the width and height of the image
+  class: {
+    type: String,
+    required: false,
+    default: undefined
+  }
+})
+
+</script>
+
+<template>
+  <div
+    class="block relative overflow-hidden min-h-full"
+    :class="props.class ?? 'w-full h-200'"
+  >
+    <img 
+      :src="props.src"
+      class="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] object-cover object-center max-w-full w-full min-h-full"
+      :srcset="props?.srcset"
+      :sizes="props?.sizes"
+      :alt="props.alt"
+    />
+  </div>
+</template>
